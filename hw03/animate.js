00938ae635f09f77f0075b055b84b43231db2cd8
bleepbloopsify <bleepbloopsify@gmail.com>
2016-02-25 00:07:29 -0500

    functional programming

--- hw03/animate.js
+++ hw03/animate.js
@@ -1,52 +1,64 @@
 var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-ctx.strokeStyle = "#FF0000";
+var circlebutton = document.getElementById('circlebutton');
+var scythebutton = document.getElementById('scythebutton');
+var stopbutton = document.getElementById('stopbutton');
+var img = new Image(100, 100);
+
+var requestId;
+
+var ctx = canvas.getContext('2d');
 ctx.fillStyle = "#FF0000";
 
-var stop = false;
-
-var radius = 0, dA = -4;
-function dCircle(){
-  if(radius >= Math.max(canvas.height, canvas.width) / 2 || radius <= 0) dA *= -1;
-  radius += dA;
+var clear = function(){
   ctx.clearRect(0,0,canvas.width, canvas.height);
-  ctx.beginPath();
-  ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2* Math.PI);
-  ctx.stroke();
-  ctx.fill();
-  var animationidcircle = window.requestAnimationFrame(dCircle);
-  if(stop) {window.cancelAnimationFrame(animationidcircle);}
-}
-
-var circleanimation = function(){
-  animationidcircle = window.requestAnimationFrame(dCircle);
-}
-
-document.getElementById('circle').addEventListener('click', circleanimation);
-document.getElementById('stop').addEventListener('click', function(){
-  console.log('hello');
-  if(stop){
-    window.requestAnimationFrame(dCircle);
-     stop = false;
-  }else {
-    stop = true;
-  }
-});
-
-
-var logo = new Image()
-logo.src = "scythe.png";
-var sx = canvas.width / 2, sy = canvas.height / 2 ;
-var dsx = 1; dsy = 1;
-var drawScythe = function(){
-  ctx.drawImage(logo, sx, sy, 50,50 );
-  if (sx >= canvas.width || sx <= 0) dsx *= -1 + (Math.random(1) - Math.random(2));
-  if(sy >= canvas.height || sy <= 0) dsy *= -1;
-  sx += dsx;
-  sy += dsy;
-  window.requestAnimationFrame(drawScythe);
 };
 
-document.getElementById('scythe').addEventListener('click', function(){
-  window.requestAnimationFrame(drawScythe);
-});
+var setupCircleAnimation = function(){
+  window.cancelAnimationFrame(requestId);
+  console.log("hi");
+  var radius = 0, dr = -3;
+  ctx.strokeStyle = "#FF0000";
+  ctx.fillStyle = "#FF0000";
+
+  var dCircle = function(){
+    clear();
+    if (radius >= Math.min(canvas.height, canvas.width) / 2 || radius <= 0) dr *= -1;
+    radius += dr;
+    ctx.beginPath();
+    ctx.arc( canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    requestId = window.requestAnimationFrame(dCircle);
+  };
+
+  dCircle();
+};
+
+var setupScytheAnimation = function(){
+  window.cancelAnimationFrame(requestId);
+
+  img.src = "scythe.png";
+  var x = canvas.width / 2, y = canvas.height / 2;
+  var dx = 4, dy = 4;
+
+  var scytheanimation = function(){
+    clear();
+    if(0 >= x || x + img.width >= canvas.width) dx *= -2 + 2 * Math.random();
+    if(0 >= y || y + img.height >= canvas.height) dy *= -2 + 2 * Math.random();
+    x += dx;
+    y += dy;
+    ctx.drawImage(img, x, y, img.width, img.height);
+
+    window.requestAnimationFrame(scytheanimation);
+  };
+
+  scytheanimation();
+};
+
+var stopAnimation = function(){
+  window.cancelAnimationFrame(requestId);
+};
+
+circlebutton.addEventListener('click', setupCircleAnimation);
+scythebutton.addEventListener('click', setupScytheAnimation);
+stopbutton.addEventListener('click', stopAnimation);
